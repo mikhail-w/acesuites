b9884f00ff1b9399ac9566215951d4abbe0d0b71
Mikhail Waddell <mikhail.waddell@gmail.com>
2023-04-09 14:22:17 -0500

    add suites page swipe

--- scripts/script.js
+++ scripts/script.js
@@ -49,3 +49,44 @@ buttons.forEach(button => {
         delete activeSlide.dataset.active
     })
 })
+
+/*
+//==================SUITES SLIDER LOGIC=================
+//SWIPER SLIDER
+var swiper = new Swiper(".bg-slider-thumbs", {
+    loop: true,
+    spaceBetween: 0,
+    slidesPerView: 0,
+    freeMode: true,
+    watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".bg-slider", {
+    loop: true,
+    spaceBetween: 0,
+    thumbs: {
+        swiper: swiper,
+    },
+});
+*/
+
+//==================SWIPER SLIDER=================
+var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+});
+
+var swiper2 = new Swiper(".mySwiper2", {
+    loop: true,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+        swiper: swiper,
+    },
+});
+
