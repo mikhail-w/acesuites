9db1dbf587f0f6fa7d1e946d88fcbf27d7011fc0
Mikhail Waddell <mikhail.waddell@gmail.com>
2023-04-14 08:21:49 -0500

    update slider thumbs

--- scripts/script.js
+++ scripts/script.js
@@ -48,37 +48,17 @@ buttons.forEach(button => {
     })
 })
 
-/*
-//==================SUITES SLIDER LOGIC=================
-//SWIPER SLIDER
-var swiper = new Swiper(".bg-slider-thumbs", {
-    loop: true,
-    spaceBetween: 0,
-    slidesPerView: 0,
-    freeMode: true,
-    watchSlidesProgress: true,
-});
-var swiper2 = new Swiper(".bg-slider", {
-    loop: true,
-    spaceBetween: 0,
-    thumbs: {
-        swiper: swiper,
-    },
-});
-*/
 
 //==================SWIPER SLIDER=================
 var swiper = new Swiper(".bg-slider-thumbs", {
     loop: true,
-    spaceBetween: 10,
-    slidesPerView: 4,
-    freeMode: true,
-    watchSlidesProgress: true,
+    spaceBetween: 0,
+    slidesPerView: 5,
 });
 
 var swiper2 = new Swiper(".bg-slider", {
     loop: true,
-    spaceBetween: 10,
+    spaceBetween: 0,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
