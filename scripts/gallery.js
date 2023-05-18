5402029f6a27afe166695ff55e8b04a637374ed5
Mikhail Waddell <mikhail.waddell@gmail.com>
2023-05-18 17:08:30 -0500

    add kingston suite

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,47 +1,64 @@
-function getSuite(val) {
-    let currSuite;
-    let temp;
-    // console.log("Val: " + val);
-    if (val === "prev") {
-        currSuite = document.getElementsByClassName("swiper-slide-prev");
-    } else {
-        currSuite = document.getElementsByClassName("swiper-slide-next");
-    }
+let suiteList = document.getElementsByClassName('suite-list');;
+let currSuite = document.getElementsByClassName('swiper-slide-active');
+// let suiteName = currSuite[0].outerText.split(' ')[0];
+let lastValIndex = suiteList.length - 1;
+let prevSuite = suiteList[lastValIndex];
+let sList = Array.from(suiteList);
 
-    // console.log(currSuite[0].outerText);
-    if (document.getElementById("status") != null) {
-        var idPost = document.getElementById("status").innerHTML;
-        temp = currSuite[0].outerText.split(' ');
-    }
-    console.log(temp);
-    let suiteName = temp[0];
-    console.log("suiteName: " + suiteName);
+//Convert the HTMLCollection to an Array and display the class list for ech element
+function listClass(sList) {
+    sList.forEach((element, index) => {
+        // console.log(element.classList);
+        suiteName = element.outerText.split(' ')[0];
+        console.log(suiteName, element.classList, index);
+
+        if (element.classList.contains('swiper-slide-active')) {
+            // element.classList.contains('swiper-slide-active')
+            console.log(suiteName, 'Zero-Index');
+            prevSuite.classList.add('slide-prev');
+        } else {
+            prevSuite.classList.remove('slide-prev');
+        }
+    });
+    console.log(' ');
 }
-// function getSuite(val) {
-//     let currSuite = document.getElementsByClassName("swiper-slide-thumb-active");
-//     console.log(currSuite);
-//     console.log(currSuite.item(0).currentSrc);
+
+// listClass(sList);
+// prevSuite.classList.add('swiper-slide-prev');
+// console.log(prevSuite);
+
+// console.log({ suiteList });
+// console.log({ lastValIndex });
+// console.log(prevSuite);
+
+// console.log(suiteName);
+
+// if (suiteName === 'Family') {
+//     console.log("At Start")
 // }
 
+function getSuite(val) {
+    if (val === "prev") {
+        if (document.getElementsByClassName("swiper-slide-prev") != null) {
+            // console.log("If One");
+            // currSuite = document.getElementsByClassName("swiper-slide-prev");
+            // console.log({ currSuite });
+            listClass(sList)
+        } else {
+            // console.log("If Two");
+            // currSuite = document.getElementsByClassName("swiper-slide-active");
+            listClass(sList);
+        }
+    } else {
+        // console.log("If Three");
+        // currSuite = document.getElementsByClassName("swiper-slide-next");
+        listClass(sList);
+    }
+    // let temp = currSuite[0].outerText.split(' ');
+    // let suiteName = temp[0];
+    // console.log(suiteName);
+}
 
-// let thumbs = document.getElementsByClassName("thumbs-container");
-// let x = thumbs[0].getElementsByTagName("img");
-// let test = Array.from(x);
-// let substring;
 
-// test.forEach(myfunction)
-
-// function myfunction(item) {
-//     // substring = getSubstring(item.src, 'suites/', '-');
-//     substring = item.src.split("/images/suites/");
-//     console.log(substring[1].split("-")[0]);
-//     // console.log(item.src);
-// }
-
-// function getSubstring(str, start, end) {
-//     char1 = str.indexOf(start) + 1;
-//     char2 = str.lastIndexOf(end);
-//     return str.substring(char1, char2);
-// }
 
 
---
+++ scripts/swipe.js
@@ -0,0 +1,53 @@
+let touchstartX = 0;
+let touchendX = 0;
+
+const delta = 6;
+let startX;
+let startY;
+
+// let gesuredZone = document.getElementById('gesuredZone');
+// // console.log('swipe');
+// function checkDirection() {
+//     if (touchendX < touchstartX) {
+//         console.log("left");
+//         if (touchstartX - touchendX > 196) {
+//             console.log('swiped left!', touchstartX, touchendX);
+//         }
+//     }
+//     if (touchendX > touchstartX) {
+//         console.log("right");
+//         if (touchendX - touchstartX > 196) {
+//             console.log('swiped right!', touchstartX, touchendX);
+//         }
+//     }
+// }
+
+// gesuredZone.addEventListener('touchstart', function (event) {
+//     touchstartX = event.changedTouches[0].screenX;
+// }, false);
+
+// gesuredZone.addEventListener('touchend', function (event) {
+//     touchendX = event.changedTouches[0].screenX;
+//     checkDirection()
+// }, false);
+
+// //Desktop click and drag action
+// gesuredZone.addEventListener('click', function (event) {
+//     console.log('mousedown');
+//     startX = event.pageX;
+// });
+
+// gesuredZone.addEventListener('mouseup', function (event) {
+//     console.log('mouseup');
+//     diffX = Math.abs(event.pageX - startX);
+//     if (diffX > delta) {
+//         alert('click');
+//     }
+// }, false);
+
+document.addEventListener('mousedown', function (event) {
+    console.log('Mousedown');
+});
+document.addEventListener('mouseup', function (event) {
+    console.log('mouseup');
+});
