74dd59962a7e4e31e82409bc1a117b05e8e0fa71
Mikhail Waddell <mikhail.waddell@gmail.com>
2023-05-20 16:01:43 -0500

    update swipe gesture response

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -25,28 +25,26 @@ function getSuite(val) {
     } else {
         currIndex = val;
     }
+    updateGallery();
+
+}
+
+function updateGallery() {
     //Change Suite Gallery Title
     selectedSuiteName = suitesListArray[currIndex].outerText.split(' ')[0];
-    galleryImageArray = Array.from(gallerySectionArray[0].getElementsByTagName('a'));
     document.getElementById("gallery-title").innerText = selectedSuiteName + " Suite Gallery";
 
+    //Update Gallery images to reflectcurrently selected suite
+    galleryImageArray = Array.from(gallerySectionArray[0].getElementsByTagName('a'));
 
+    //cycle through all <a> tags and update the href and image src paths
     galleryImageArray.forEach((item, index) => {
         hrefPath = item.href.split('suites/');
         srcPath = item.childNodes[1].src.split('suites/');
-        // console.log(item);
-        // console.log(index, "HREF: " + item.href);
-        // console.log(index, "Image Source: " + item.childNodes[1].src);
-        // console.log(srcPath);
-        // console.log(hrefPath[0]);
         let newPath = hrefPath[0] + "suites/" + selectedSuiteName.toLowerCase() + "/" + selectedSuiteName.toLowerCase() + "-0" + (index + 1) + ".jpg";
         item.href = newPath;
         item.childNodes[1].src = srcPath[0] + "suites/" + selectedSuiteName.toLowerCase() + "/" + selectedSuiteName.toLowerCase() + "-0" + (index + 1) + ".jpg";
-        // console.log(newPath);
-
     })
 }
-
-
 
 
