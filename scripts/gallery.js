9ed8b54576affe1dc3ff800513f288b760c77ad2
Mikhail Waddell <mikhail.waddell@gmail.com>
2023-07-02 12:56:52 -0500

    update swipe

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -11,29 +11,33 @@ let indexMap = new Map([
 let selectedSuiteName;
 let hrefPath;
 let srcPath;
-let curTitle = Array.from(document.getElementsByClassName('suite-item'));
 
-console.log("Current Index: ", currIndex);
-// console.log(suitesListArray);
-// console.log(gallerySectionArray);
-// console.log("Loaded")
-// console.log(curTitle);
+//Swipe jesture variables
+let touchstartX = 0;
+let touchendX = 0;
+let gesturedZone = document.getElementById('gesturedZone');
+
+//Swipe logic
+gesturedZone.addEventListener('touchstart', function (event) {
+    touchstartX = event.changedTouches[0].screenX;
+}, false);
+
+gesturedZone.addEventListener('touchend', function (event) {
+    touchendX = event.changedTouches[0].screenX;
+    getActive();
+}, false);
+
+
 //This function identifies the currently selected suite
 function getSuite(val) {
-
     if (isNaN(val)) {
-
         if (val === "prev") {
-            // console.log("In 'getSuite' if statement")
-            // console.log(curTitle);
             if (currIndex === 0) {
                 currIndex = 3
             } else {
                 currIndex -= 1;
             }
         } else {
-            // console.log("In 'getSuite' else statement")
-            // console.log(curTitle);
             if (currIndex === 3) {
                 currIndex = 0;
             } else {
@@ -43,43 +47,39 @@ function getSuite(val) {
     } else {
         //If suite item is selected via thumbs-container
         currIndex = val;
-        // console.log("In 'getSuite' last else statement")
-        // console.log(curTitle);
     }
-    console.log("Getsuite Current Index: ", currIndex);
+    // console.log("Getsuite Current Index: ", currIndex);
     updateImageGallery();
 
 }
 
 function getActive() {
-    //Get Folder names for each hero image
-    // console.log(curTitle);
-    // console.log("current index: ", currIndex, suitesListArray[currIndex]);
     let srcName;
-    curTitle.forEach((titleItem) => {
+    suitesListArray.forEach((titleItem) => {
         let curSrc = titleItem.children[0].src.split('/');
         if (titleItem.classList.contains('swiper-slide-active')) {
             srcName = curSrc[curSrc.length - 1].split('-')[0];
-            // console.log("Get Active Function: ", srcName.charAt(0).toUpperCase() + srcName.slice(1));
             document.getElementById("gallery-title").innerText = srcName.charAt(0).toUpperCase() + srcName.slice(1) + " Suite Gallery";
         }
     })
-    galleryImageArray = Array.from(gallerySectionArray[0].getElementsByTagName('a'));
+    // galleryImageArray = Array.from(gallerySectionArray[0].getElementsByTagName('a'));
     //cycle through all <a> tags and update the href and image src paths
-    galleryImageArray.forEach((item, index) => {
-        hrefPath = item.href.split('suites/');
-        srcPath = item.childNodes[1].src.split('suites/');
-        let newPath = hrefPath[0] + "suites/" + srcName + "/" + srcName + "-0" + (index + 1) + ".jpg";
-        item.href = newPath;
-        item.childNodes[1].src = srcPath[0] + "suites/" + srcName + "/" + srcName + "-0" + (index + 1) + ".jpg";
-    })
-    console.log(suitesListArray);
+
+    // galleryImageArray.forEach((item, index) => {
+    //     hrefPath = item.href.split('suites/');
+    //     srcPath = item.childNodes[1].src.split('suites/');
+    //     let newPath = hrefPath[0] + "suites/" + srcName + "/" + srcName + "-0" + (index + 1) + ".jpg";
+    //     item.href = newPath;
+    //     item.childNodes[1].src = srcPath[0] + "suites/" + srcName + "/" + srcName + "-0" + (index + 1) + ".jpg";
+    // })
+
     currIndex = indexMap.get(srcName);
     console.log("GetActive Current Index: ", currIndex);
 }
 
 function updateImageGallery() {
     //Change Suite Gallery Title
+    console.log("updateImageGallery Current Index: ", currIndex);
     selectedSuiteName = suitesListArray[currIndex].outerText.split(' ')[0];
     // console.log("Currently Selected Suite Name:", selectedSuiteName);
     document.getElementById("gallery-title").innerText = selectedSuiteName + " Suite Gallery";
@@ -96,5 +96,4 @@ function updateImageGallery() {
         item.childNodes[1].src = srcPath[0] + "suites/" + selectedSuiteName.toLowerCase() + "/" + selectedSuiteName.toLowerCase() + "-0" + (index + 1) + ".jpg";
     })
 }
-
 
