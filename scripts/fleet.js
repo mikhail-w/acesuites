bf83d0d78880b8b8713e02a80dfbe0a0fba952d5
Mikhail Waddell <mikhail.waddell@gmail.com>
2023-07-08 17:24:31 -0500

    update fleet page

--- scripts/fleet.js
+++ scripts/fleet.js
@@ -1,3 +1,25 @@
+
+
+//Set new date
+let curDate = document.getElementById('dates').getAttribute('value');
+console.log(curDate);
+date = new Date();
+year = date.getFullYear();
+month = date.getMonth() + 1;
+day = date.getDate();
+curDate = month + "/" + day + "/" + year;
+
+//Data Range Calender Picker
+$(function selectDates() {
+    console.log("SelectDates Function");
+    $('input[name="daterange"]').daterangepicker({
+        opens: 'left'
+    }, function (start, end, label) {
+        console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+    });
+});
+
+//Image Swiper
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     spaceBetween: 30,
